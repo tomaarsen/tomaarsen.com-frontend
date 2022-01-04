340fc29b6715e3fcc9be9ee167d88d3177e98d30
Tom Aarsen <Cubiegamedev@gmail.com>
2022-01-04 23:06:18 +0100

    Prevent overflow of nltk/usage/list on small screens including mobile

--- src/components/projects/nltk/UsageList.js
+++ src/components/projects/nltk/UsageList.js
@@ -21,13 +21,13 @@ class UsagePlot extends React.Component {
         return (
             <div className="box" style={{ maxHeight: "100%", overflowY: "auto" }}>
                 <div style={{ textAlign: "center" }}>Cumulative usage of objects from the Natural Language Toolkit (NLTK)</div>
-                <table className="table">
+                <table className="table" style={{ maxWidth: "100%", tableLayout: "fixed" }}>
                     <thead className="thead-light">
                         <tr>
-                            <th scope="col" style={{ width: "100%" }} >
+                            <th scope="col">
                                 Object
                             </th>
-                            <th scope="col">
+                            <th scope="col" style={{ width: "6rem" }}>
                                 Usage
                             </th>
                         </tr>
@@ -35,12 +35,12 @@ class UsagePlot extends React.Component {
                     <tbody>
                         {this.state.uses.map(obj_tuple => {
                             // obj_tuple is (obj, occ)
-                            return <tr>
-                                <td>
-                                    <span className="input-group-text code">{obj_tuple[0]}</span>
+                            return <tr key={obj_tuple[0]}>
+                                <td style={{ overflowX: "auto" }}>
+                                    <div className="input-group-text code" style={{ overflowX: "auto" }} >{obj_tuple[0]}</div>
                                 </td>
                                 <td>
-                                    <input className="code" type="text" value={obj_tuple[1]} size="7" aria-label="Occurrence" readOnly />
+                                    <input style={{ padding: "0.375rem 0.5rem;", backgroundColor: "white" }} className="form-control code" type="text" value={obj_tuple[1]} size="6" aria-label="Occurrence" readOnly />
                                 </td>
                             </tr>
                         })}

b383860db05b835bef8d112547c273b82c55baad
Tom Aarsen <Cubiegamedev@gmail.com>
2022-01-04 16:24:54 +0100

    Deduplicate from utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,20 +1,7 @@
 
 // Example POST method implementation:
 async function postJson(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
+    const response = await post(url, data);
     return response.json(); // parses JSON response into native JavaScript objects
 }
 
@@ -34,7 +21,7 @@ async function post(url = '', data = {}) {
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    return response; // parses JSON response into native JavaScript objects
+    return response;
 }
 
 export { postJson, post };

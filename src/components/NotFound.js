57981b40d8138d22d40d3200923c957b1cc723c8
Tom Aarsen <Cubiegamedev@gmail.com>
2022-02-04 10:49:41 +0100

    Add animated 404 page, because it's fun

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
+import "../css/not_found.css";
+
 class NotFound extends React.Component {
     render() {
-        return(
-            <div className="box">
-                <h4>404 - Not Found!</h4>
-                <hr />
-                <NavLink to="/home">Go Home</NavLink>  
+        return (
+            <div class="dvd_container">
+                <div className="box" style={{ width: "max-content" }}>
+                    <h4>404 - Not Found!</h4>
+                    <hr />
+                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
+                </div>
+
+                <div className="box delay" style={{ width: "max-content" }}>
+                    <h4>404 - Not Found!</h4>
+                    <hr />
+                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
+                </div>
+
+                <div className=" box large_delay" style={{ width: "max-content" }}>
+                    <h4>404 - Not Found!</h4>
+                    <hr />
+                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
+                </div>
             </div>
         )
     }

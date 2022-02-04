a3ef0856ab4d0ea181ccaaf04b851801e5c555ad
Tom Aarsen <Cubiegamedev@gmail.com>
2022-02-04 11:14:30 +0100

    Make entire 404 box a link to home

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -7,23 +7,29 @@ class NotFound extends React.Component {
     render() {
         return (
             <div class="dvd_container">
-                <div className="box" style={{ width: "max-content" }}>
-                    <h4>404 - Not Found!</h4>
-                    <hr />
-                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
-                </div>
+                <NavLink className="dark" to="/home">
+                    <div className="box" style={{ width: "max-content" }}>
+                        <h4>404 - Not Found!</h4>
+                        <hr />
+                        <a href="/home">Go Home</a>, you're drunk.
+                    </div>
+                </NavLink>
 
-                <div className="box delay" style={{ width: "max-content" }}>
-                    <h4>404 - Not Found!</h4>
-                    <hr />
-                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
-                </div>
+                <NavLink className="dark" to="/home">
+                    <div className="box delay" style={{ width: "max-content" }}>
+                        <h4>404 - Not Found!</h4>
+                        <hr />
+                        <a href="/home">Go Home</a>, you're drunk.
+                    </div>
+                </NavLink>
 
-                <div className=" box large_delay" style={{ width: "max-content" }}>
-                    <h4>404 - Not Found!</h4>
-                    <hr />
-                    <NavLink to="/home">Go Home</NavLink>, you're drunk.
-                </div>
+                <NavLink className="dark" to="/home">
+                    <div className="box large_delay" style={{ width: "max-content" }}>
+                        <h4>404 - Not Found!</h4>
+                        <hr />
+                        <a href="/home">Go Home</a>, you're drunk.
+                    </div>
+                </NavLink>
             </div>
         )
     }

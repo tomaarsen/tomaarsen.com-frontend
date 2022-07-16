b1df1c6d57a4ddf854c21b77a1424eddff18816c
Tom Aarsen <Cubiegamedev@gmail.com>
2022-07-17 01:15:51 +0200

    Attempt to split up built packages using React's lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
     Route,
     BrowserRouter,
@@ -10,7 +10,6 @@ import Header from "./Header";
 import NotFound from "./NotFound";
 
 import Try from "./projects/inflex/Try";
-import Performance from "./projects/inflex/Performance";
 import Readme from "./projects/Readme";
 import Overview from "./projects/Overview";
 
@@ -22,6 +21,10 @@ import AboutPage from "./about/AboutPage";
 import "../css/color.css";
 import "../css/fonts.css";
 import "../css/App.css";
+
+// Load the expensive Performance tab lazily,
+// allowing it to be built into a separate package
+const Performance = lazy(() => import("./projects/inflex/Performance"));
 
 class Main extends React.Component {
 
@@ -46,33 +49,35 @@ class Main extends React.Component {
                     <Header />
                     <Sidebar repos={this.state.repos} />
                     <div className="content h-100" style={{ overflow: "hidden" }}>
-                        <Switch>
-                            <Route exact path="/">
-                                <Redirect to="/home" />
-                            </Route>
-                            <Route exact path="/projects">
-                                <Overview repos={this.state.repos} />
-                            </Route>
-                            <Route exact path="/projects/inflex/try" component={Try} />
-                            <Route exact path="/projects/inflex/performance" component={Performance} />
-                            {this.state.repos.map(repo => {
-                                return (
-                                    <Route exact path={`/projects/${repo['name']}`} key={repo.id}>
-                                        <Readme repo={repo} />
-                                    </Route>
-                                )
-                            })}
-                            <Route exact path="/home" component={AboutPage} />
-                            <Route exact path="/projects/nltk/usage">
-                                <Redirect to="/projects/nltk/usage/plot" />
-                            </Route>
-                            <Route exact path="/projects/nltk/usage/plot" component={UsagePlot} />
-                            <Route exact path="/projects/nltk/usage/list" component={UsageList} />
+                        <Suspense fallback={<h1>Loading...</h1>}>
+                            <Switch>
+                                <Route exact path="/">
+                                    <Redirect to="/home" />
+                                </Route>
+                                <Route exact path="/projects">
+                                    <Overview repos={this.state.repos} />
+                                </Route>
+                                <Route exact path="/projects/inflex/try" component={Try} />
+                                <Route exact path="/projects/inflex/performance" component={Performance} />
+                                {this.state.repos.map(repo => {
+                                    return (
+                                        <Route exact path={`/projects/${repo['name']}`} key={repo.id}>
+                                            <Readme repo={repo} />
+                                        </Route>
+                                    )
+                                })}
+                                <Route exact path="/home" component={AboutPage} />
+                                <Route exact path="/projects/nltk/usage">
+                                    <Redirect to="/projects/nltk/usage/plot" />
+                                </Route>
+                                <Route exact path="/projects/nltk/usage/plot" component={UsagePlot} />
+                                <Route exact path="/projects/nltk/usage/list" component={UsageList} />
 
-                            {/* Catch all - redirect to 404 */}
-                            <Route path="/404" exact={true} component={NotFound} />
-                            {this.state.repos.length > 0 && <Redirect to="/404" />}
-                        </Switch>
+                                {/* Catch all - redirect to 404 */}
+                                <Route path="/404" exact={true} component={NotFound} />
+                                {this.state.repos.length > 0 && <Redirect to="/404" />}
+                            </Switch>
+                        </Suspense>
                     </div>
                 </div>
             </BrowserRouter>

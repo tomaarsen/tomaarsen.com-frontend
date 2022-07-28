c998a1e62feac5b33ded66486f8870f3645cd398
Tom Aarsen <Cubiegamedev@gmail.com>
2022-07-28 11:37:14 +0200

    Apply server-side caching of README pages for faster page loads

--- src/components/projects/Readme.js
+++ src/components/projects/Readme.js
@@ -3,56 +3,49 @@ import showdown from "showdown";
 import ReactHtmlParser from 'react-html-parser';
 
 import ReadmeCard from "./ReadmeCard";
+import { useQuery } from "react-query";
 
 import "../../css/readme.css";
 
-class Readme extends React.Component {
+const CONVERTER = new showdown.Converter(
+    {
+        tables: true,
+        tasklists: true,
+        simplifiedAutoLink: true,
+        ghMentions: true,
+        openLinksInNewWindow: true,
+        splitAdjacentBlockquotes: true
+    });
+CONVERTER.setFlavor('github');
 
-    constructor() {
-        super();
-        this.converter = new showdown.Converter(
-            {
-                tables: true,
-                tasklists: true,
-                simplifiedAutoLink: true,
-                ghMentions: true,
-                openLinksInNewWindow: true,
-                splitAdjacentBlockquotes: true
-            });
-        this.converter.setFlavor('github');
-        this.state = {
-            convertedMarkdown: ""
-        };
+function Readme(props) {
+    let { isLoading, data } = useQuery([`${props.repo.full_name}Readme`], () =>
+        fetch(`https://raw.githubusercontent.com/${props.repo.full_name}/${props.repo.default_branch}/README.md`)
+            .then(res => res.text())
+    )
+    if (isLoading) {
+        data = "";
     }
 
-    componentDidMount() {
-        // TODO: Cache this API call
-        fetch(`https://raw.githubusercontent.com/${this.props.repo.full_name}/${this.props.repo.default_branch}/README.md`)
-            .then(r => r.text())
-            .then(r => this.setState({
-                convertedMarkdown:
-                    "<div class='box'>" +
-                    this.converter.makeHtml(r)
-                        .replaceAll("<hr />", "</div><div class='box'>")
-                        // .replaceAll("<hr />", "")
-                        // .replaceAll("<h1 ", "</div><div class='box'><h1 ")
-                        // .replaceAll("<h2 ", "</div><div class='box'><h2 ")
-                        // .replaceAll("<h3 ", "</div><div class='box'><h3 ")
-                        .replaceAll("<pre>\n", "<pre>")
-                    // .replaceAll("<br><br>", "<br>")
-                    +
-                    "</div>"
-            }));
-    }
+    let htmlMarkdown = (
+        "<div class='box'>"
+        + CONVERTER.makeHtml(data)
+            .replaceAll("<hr />", "</div><div class='box'>")
+        // .replaceAll("<hr />", "")
+        // .replaceAll("<h1 ", "</div><div class='box'><h1 ")
+        // .replaceAll("<h2 ", "</div><div class='box'><h2 ")
+        // .replaceAll("<h3 ", "</div><div class='box'><h3 ")
+        // .replaceAll("<pre>\n", "<pre>")
+        // .replaceAll("<br><br>", "<br>")
+        + "</div>"
+    )
 
-    render() {
-        return (
-            <div className="row-grid">
-                <ReadmeCard {...this.props.repo}></ReadmeCard>
-                {ReactHtmlParser(this.state.convertedMarkdown)}
-            </div>
-        )
-    }
+    return (
+        <div className="row-grid">
+            <ReadmeCard {...props.repo}></ReadmeCard>
+            {ReactHtmlParser(htmlMarkdown)}
+        </div>
+    )
 }
 
 // TODO: Add papers of SPLCompiler
